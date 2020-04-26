94262796d3fd7e62f9830e16991c2bb7c3530929
Friends-Games <64155278+Friends-Games@users.noreply.github.com>
2020-04-26 16:50:03 -0400

    Add files via upload

---
+++ JungleDirtTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="JungleDirtTileSet" tilewidth="32" tileheight="32" tilecount="42" columns="7">
+ <image source="Sprites/Tiles/JungleDirtTileSet.png" width="224" height="192"/>
+</tileset>
